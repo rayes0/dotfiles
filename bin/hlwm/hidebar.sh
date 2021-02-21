d6ed0f5ea6df39663946b3be5b52c81beff79988
rayes <70722312+rayes0@users.noreply.github.com>
2021-02-21 14:35:13 -0700

    Update bar stuff

--- bin/hlwm/hidebar.sh
+++ bin/hlwm/hidebar.sh
@@ -6,14 +6,14 @@ number=$(herbstclient list_monitors | wc -l)
 if pgrep -x herbbar; then
 	pkill herbbar
 else
-	case $number in                     
-    1)                              
-        herbbar | lemonbar -n bar -B \#a5f7f7f7 -F \#585858 -f 'SF Mono:size=11' -f 'Noto Serif CJK JP:style=SemiBold:size=10' -f 'Noto Sans CJK JP:size=10' -g 1920x30+0+0 | sh & ;;
-    2)                             
-        herbbar | lemonbar -n bar -B \#a5f7f7f7 -F \#585858 -f 'SF Mono:size=11' -f 'Noto Serif CJK JP:style=SemiBold:size=10' -f 'Noto Sans CJK JP:size=10' -g 1920x30+0+0 | sh &
-        herbbar 2 | lemonbar -n bar -B \#a5f7f7f7 -F \#585858 -f 'SF Mono:size=11' -f 'Noto Serif CJK JP:style=SemiBold:size=10' -f 'Noto Sans CJK JP:size=10' -g 1920x30+1920+0 | sh & ;;
-    *)                                    
-        herbbar | lemonbar -n bar -B \#a5f7f7f7 -F \#585858 -f 'SF Mono:size=11' -f 'Noto Serif CJK JP:style=SemiBold:size=10' -f 'Noto Sans CJK JP:size=10' -g 1920x30+0+0 | sh & ;;
+	case $number in
+		1)
+			herbbar | lemonbar -n bar -B \#a5ede6e3 -F \#6c605a -f 'SF Mono:size=11' -f 'Noto Serif CJK JP:style=SemiBold:size=10' -f 'Noto Sans CJK JP:size=10' -g 1920x30+0+0 | sh & ;;
+		2)
+			herbbar | lemonbar -n bar -B \#a5ede6e3 -F \#6c605a -f 'SF Mono:size=11' -f 'Noto Serif CJK JP:style=SemiBold:size=10' -f 'Noto Sans CJK JP:size=10' -g 1920x30+0+0 | sh &
+			herbbar 2 | lemonbar -n bar -B \#a5ede6e3 -F \#6c605a -f 'SF Mono:size=11' -f 'Noto Serif CJK JP:style=SemiBold:size=10' -f 'Noto Sans CJK JP:size=10' -g 1920x30+1920+0 | sh & ;;
+		*)
+			herbbar | lemonbar -n bar -B \#a5ede6e3 -F \#6c605a -f 'SF Mono:size=11' -f 'Noto Serif CJK JP:style=SemiBold:size=10' -f 'Noto Sans CJK JP:size=10' -g 1920x30+0+0 | sh & ;;
 	esac
 fi
 
--- bin/utilities/netstartup.sh
+++ bin/utilities/netstartup.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-vpn.sh &
-wait
-
-if [[ $(protonvpn status) == *"Status:       Connected"* ]];then
+start_programs() {
 	urxvt -e weechat &
 	ankisync &
 	#virsh start fedora32 &
 	#virt-viewer fedora32 &
 	#sleep 5
 	#whalebird &
+}
+
+if [[ $(protonvpn status) == *"Status:       Connected"* ]];then
+	start_programs
+else
+	vpn.sh &
+	wait
+	start_programs
 fi
