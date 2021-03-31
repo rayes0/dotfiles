26dbbf6f2ab08f078edb1b4022c92c55e62630e5
rayes <70722312+rayes0@users.noreply.github.com>
2021-03-31 14:51:03 -0600

    Fix terminal not spawning correctly

--- .config/eww/scripts/cmus-check.sh
+++ .config/eww/scripts/cmus-check.sh
@@ -2,7 +2,7 @@
 # This is only run when the eww window is open
 
 cmus_stats="$(cmus-remote -Q)" || exit 0
-status="$(echo "$cmus_stats" | grep "status " | cut -d' ' -f2)"
+#status="$(echo "$cmus_stats" | grep "status " | cut -d' ' -f2)"
 
 changetomin() { printf "%d:%02d" "$((10#$1 / 60))" "$((10#$1 % 60))"; }
 
