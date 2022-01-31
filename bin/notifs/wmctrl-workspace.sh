a1379ee14d4daebc47362fa7123e7a68146d7899
rayes <mailj2@protonmail.com>
2022-01-31 13:52:30 -0700

    Make xmonad better, and also delete obselete mpv stuff

--- bin/notifs/wmctrl-workspace.sh
+++ bin/notifs/wmctrl-workspace.sh
@@ -7,8 +7,7 @@ check_ws() {
   IFS=', ' read -ra names <<< "$(xprop -root -notype _NET_DESKTOP_NAMES | cut -d'=' -f2 | xargs)"
   current_name="${names[current_id]}"
   readarray -t has_windows_id <<< "$(wmctrl -l | cut -d' ' -f3)"
-  echo "${has_windows_id[@]}"
-
+  
   has_windows=()
   for i in "${has_windows_id[@]}"; do
     has_windows+=("${names[i]}")
@@ -25,25 +24,21 @@ check_ws() {
   done
 }
 
-check_ws
-dunstify -p -i xpad -h string:x-canonical-private-synchronous:barless-info "Workspaces" "$notif_string"
+query() {
+  #   watch -tn 0.2 'xprop -root -notype _NET_CURRENT_DESKTOP' &
+  while sleep 0.1; do
+    [[ $(xprop -root -notype _NET_CURRENT_DESKTOP | cut -d'=' -f2 | xargs) -ne $current_id ]] && check_and_send
+  done &
+  wpid=$!
+  sleep 7
+  kill $wpid
+}
 
-# query() {
-#   watch -tn 0.2 'xprop -root -notype _NET_CURRENT_DESKTOP' &
-#   while sleep 0.2; 
-#   wpid=$!
-#   echo $wpid
-#   sleep 7
-#   kill $wpid
-# }
+check_and_send() {
+  check_ws
+  id=$(dunstify -p -i xpad -h string:x-canonical-private-synchronous:barless-info "Workspaces" "$notif_string")
+}
 
-# check_ws
-# id1=$(dunstify -p -i xpad -h string:x-canonical-private-synchronous:barless-info "Workspaces" "$notif_string")
-
-# while read -r; do
-#   check_ws
-#   id2=$(dunstify -p -i xpad -h string:x-canonical-private-synchronous:barless-info "Workspaces" "$notif_string")
-# done < <(query)
-
-# [ -n "$id1" ] && dunstify -C "$id1"
-# [ -n "$id2" ] && dunstify -C "$id2"
+check_and_send
+query
+dunstify -C "$id"
